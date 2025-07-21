64018e83c2748712bb99a3a12dbddad3270e835b
yash rajput <rajputyash774@gmail.com>
2025-07-22 03:10:53 +0530

    redux toolkit configured

--- react chai aur code/13MegaBlogAppwrite/src/App.jsx
+++ react chai aur code/13MegaBlogAppwrite/src/App.jsx
@@ -1,12 +1,38 @@
-
+import React ,{useState,useEffect, use} from 'react';
+import { Header, Footer } from './components';
+import {useDispatch} from 'react-redux';
+import authService from './appwrite/auth';
+import { login,logout } from './store/authSlice';
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);  
+  const [isLoading, setIsLoading] = useState(true);
+  const dispatch = useDispatch();
 
-  return (
-    <>
-      <h1 className="bg-gray-600 text-white text-2xl text-center">Mega-Blog</h1>
-    </>
+  useEffect(() => {
+    authService.getCurrectUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login(userData));
+      }else{
+        dispatch(logout());
+      }
+    })
+    .finally(() => setIsLoading(false));
+  },[]);
+ 
+
+  return !isLoading ? (
+    <div className='min-h-screen bg-gray-500 flex flex-wrap content-between'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) :(
+    <div className='min-h-screen bg-amber-950'></div>
   )
 }
 
