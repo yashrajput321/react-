8f56c9a9398c9f6b4edc8e24f77a624ca000effc
yash rajput <rajputyash774@gmail.com>
2026-03-06 01:12:55 +0530

    Add styled-components and implement Header and Leaderboard components

---
+++ react hooks/API call with hooks/src/Components/Leaderboard/index.jsx
@@ -0,0 +1,12 @@
+import {LeaderboardContainer} from './styledComponents'
+
+const Leaderboard = () => {
+  // Your code goes here...
+  const renderLeaderboard = () => {
+    // Your code goes here...
+  }
+
+  return <LeaderboardContainer>{renderLeaderboard()}</LeaderboardContainer>
+}
+
+export default Leaderboard
