2755827d60fb3fb8bca5f2867d0dacc3765d28aa
yash rajput <rajputyash774@gmail.com>
2026-03-21 16:13:25 +0530

    feat: implement Content component and update App to use it

---
+++ motion framer/framer1/src/components/Content/index.jsx
@@ -0,0 +1,8 @@
+
+export default Content=() => {
+    return(
+        <div className="content bg-neutral-950 h-screen w-full items-center justify-center">
+            <button className="text-neutral-500">hello</button>
+        </div>
+    )
+}
